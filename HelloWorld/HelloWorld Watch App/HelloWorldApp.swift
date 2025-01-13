4eba3e1209916b027d23cf69448325952691cd7b
yungroms <117047802+yungroms@users.noreply.github.com>
2025-01-13 13:53:36 +0000

    Add files via upload

---
+++ HelloWorld/HelloWorld Watch App/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld Watch App
+//
+//  Created by Morris-Stiff R O (FCES) on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
